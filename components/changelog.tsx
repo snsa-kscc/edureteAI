9b1fff16bda551ad75b11f1277070415b714082b
snsa-kscc <sinisa@dvasadva.com>
2024-05-14 05:01:11 +0200

    edit changelog

--- components/changelog.tsx
+++ components/changelog.tsx
@@ -21,6 +21,7 @@ export function Changelog() {
       <AlertDialogContent>
         <AlertDialogHeader>
           <AlertDialogTitle>What is new in the app</AlertDialogTitle>
+          <AlertDialogDescription>14.05.2023 - Added OpenAI's GPT-4o model</AlertDialogDescription>
           <AlertDialogDescription>02.05.2023 - Added chat history and system prompt. Everything still in beta. 2DO: bug fixes</AlertDialogDescription>
           <AlertDialogDescription>19.04.2023 - Added flagship OpenAI/GPT4 Turbo model in the dropdown menu</AlertDialogDescription>
         </AlertDialogHeader>
