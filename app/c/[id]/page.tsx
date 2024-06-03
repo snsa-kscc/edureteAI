176da724f4ede169f9a7bf249f1370547adbeedb
snsa-kscc <sinisa@dvasadva.com>
2024-06-03 10:59:34 +0200

    add default models

--- app/c/[id]/page.tsx
+++ app/c/[id]/page.tsx
@@ -9,7 +9,8 @@ import { getChat } from "@/lib/actions";
 import { Sidebar } from "@/components/sidebar";
 
 const DEFAULT_SYSTEM_PROMPT = `You are a reasoning AI tasked with solving the user's math-based questions. Logically arrive at the solution, and be factual. In your answers, clearly detail the steps involved and give the final answer. If you can't solve the question, say "I don't know". When responding with math formulas in the response, you must write the formulae using only Unicode from the Mathematical Operators block and other Unicode symbols. The AI GUI render engine does not support TeX code. You must not use LaTeX in responses.`;
-const DEFAULT_MODEL = "gpt-3.5-turbo";
+const DEFAULT_LEFT_MODEL = "gpt-4o";
+const DEFAULT_RIGHT_MODEL = "claude-3-opus-20240229";
 
 export default async function cPage({ params }: { params: { id: string } }) {
   const { userId } = auth();
@@ -30,24 +31,24 @@ export default async function cPage({ params }: { params: { id: string } }) {
             chatId: params.id,
             chatAreaId: "left",
             messages: chat?.leftMessages ?? [],
-            model: chat?.leftModel ?? DEFAULT_MODEL,
+            model: chat?.leftModel ?? DEFAULT_LEFT_MODEL,
             system: chat?.leftSystemPrompt ?? DEFAULT_SYSTEM_PROMPT,
           }}
           initialUIState={[]}
         >
-          <Chat id={params.id} initialModel={chat?.leftModel ?? DEFAULT_MODEL} initialSystem={chat?.leftSystemPrompt ?? DEFAULT_SYSTEM_PROMPT} />
+          <Chat id={params.id} initialModel={chat?.leftModel ?? DEFAULT_LEFT_MODEL} initialSystem={chat?.leftSystemPrompt ?? DEFAULT_SYSTEM_PROMPT} />
         </AI>
         <AI
           initialAIState={{
             chatId: params.id,
             chatAreaId: "right",
             messages: chat?.rightMessages ?? [],
-            model: chat?.rightModel ?? DEFAULT_MODEL,
+            model: chat?.rightModel ?? DEFAULT_RIGHT_MODEL,
             system: chat?.rightSystemPrompt ?? DEFAULT_SYSTEM_PROMPT,
           }}
           initialUIState={[]}
         >
-          <Chat id={params.id} initialModel={chat?.rightModel ?? DEFAULT_MODEL} initialSystem={chat?.rightSystemPrompt ?? DEFAULT_SYSTEM_PROMPT} />
+          <Chat id={params.id} initialModel={chat?.rightModel ?? DEFAULT_RIGHT_MODEL} initialSystem={chat?.rightSystemPrompt ?? DEFAULT_SYSTEM_PROMPT} />
         </AI>
       </div>
     </main>
