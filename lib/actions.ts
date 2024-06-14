7ad38d49473cc6a31dbf36d68e1b4ae273c0944c
snsa-kscc <sinisa@dvasadva.com>
2024-06-14 14:52:10 +0200

    add edge

--- lib/actions.ts
+++ lib/actions.ts
@@ -66,7 +66,6 @@ export async function saveChat(chat: Chat) {
   pipeline.sadd(`userIds`, chat.userId);
 
   await pipeline.exec();
-  console.log(`hello from ${process.env.VERCEL_REGION}`);
 }
 
 export async function removeChat({ id, path, userId }: { id: string; path: string; userId: string | null }) {
