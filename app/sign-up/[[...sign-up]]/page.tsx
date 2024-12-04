7cb6f139689a0b9eb877e075f292b9d6caa42a01
snsa-kscc <sven1397@hotmail.com>
2024-12-04 03:14:52 +0100

    edit middleware

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <SignUp path="/sign-up" />
+      <SignUp />
     </div>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,6 @@ const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 const isDashboardRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
-  if (request.nextUrl.pathname.startsWith("/api")) {
-    return NextResponse.next();
-  }
-
   if (!isPublicRoute(request)) {
     await auth.protect();
 
@@ -22,5 +18,10 @@ export default clerkMiddleware(async (auth, request) => {
 });
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next|favicon.ico).*)", "/", "/(trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
