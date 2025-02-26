17aba893ffa83480a137e2e95049674b36a7a91d
snsa-kscc <sinisa@dvasadva.com>
2025-02-26 21:20:02 +0100

    edit middleware

--- middleware.ts
+++ middleware.ts
@@ -5,11 +5,17 @@ const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 const isDashboardRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
+  const { sessionClaims } = await auth();
+  const userId = sessionClaims?.userId;
+
+  if (!userId && !isPublicRoute(request)) {
+    return (await auth()).redirectToSignIn();
+  }
+
   if (!isPublicRoute(request)) {
     await auth.protect();
 
     if (isDashboardRoute(request)) {
-      const { sessionClaims } = await auth();
       if (sessionClaims?.membership && Object.keys(sessionClaims.membership).length === 0) {
         return NextResponse.rewrite(new URL("/404", request.url));
       }
