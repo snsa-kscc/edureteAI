b34d103cc74e52f15290a25bc08497a20e564769
snsa-kscc <sinisa@dvasadva.com>
2025-03-06 09:59:44 +0100

    add dynamic root route

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { v4 as uuidv4 } from "uuid";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   redirect(`/c/${uuidv4()}`);
 }
