e00a66d0154f1e270cb0d84687f8d5dedf54209f
snsa-kscc <sinisa@dvasadva.com>
2025-03-08 16:32:44 +0100

    edit signup.tsx

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -5,9 +5,10 @@ type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 export default async function SignUpPage(props: { searchParams: SearchParams }) {
   const searchParams = await props.searchParams;
   const referrerId = searchParams.ref;
+
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <SignUp forceRedirectUrl={`/api/process-referral?ref=${referrerId}`} />
+      {referrerId ? <SignUp forceRedirectUrl={`/api/process-referral?ref=${referrerId}`} /> : <SignUp />}
     </div>
   );
 }
