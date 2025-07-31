8c0dfb5865390a4a104b949ee12ef38948ba4846
snsa-kscc <sinisa@dvasadva.com>
2025-07-31 20:02:46 +0200

    chore: update Stripe API version to 2025-06-30.basil

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,6 +5,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-03-31.basil",
+  apiVersion: "2025-06-30.basil",
   typescript: true,
 });
